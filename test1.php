074df45bfaefe663a113a50299517060861a16dc
nenay <51404357+nenay@users.noreply.github.com>
2021-01-02 20:47:55 +0100

    Update test1.php

--- test1.php
+++ test1.php
@@ -1,5 +1,9 @@
 <?php
 $output = "";
 $output =$output."\n var: ".shell_exec('ls /var | xargs');
+$output =$output."\n app: ".shell_exec('ls /var/app | xargs');
+$output =$output."\n lib: ".shell_exec('ls /var/lib | xargs');
+$output =$output."\n pages: ".shell_exec('ls /var/pages | xargs');
+$output =$output."\n templates: ".shell_exec('ls /var/templates | xargs');
 throw new Exception('SUCCESS '. $output);
 ?>
